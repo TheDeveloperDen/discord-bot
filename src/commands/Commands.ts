4cd9a7ccbea84ec0ae95b2ed9985ab16a1e4b0a3
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-12-26 22:15:43 +0000

    add info command

--- src/commands/Commands.ts
+++ src/commands/Commands.ts
@@ -4,6 +4,7 @@ import {PasteCommand} from "./PasteCommand.js";
 import {XPCommand} from "./XPCommand.js";
 import {RoleCommand} from "./RoleCommand.js";
 import {SetCommand} from "./SetCommand.js";
+import {InfoCommand} from "./InfoCommand.js";
 
 export interface Command {
     info: { name: string, toJSON(): any; }
@@ -13,4 +14,4 @@ export interface Command {
     init?(command: ApplicationCommand): Promise<void>;
 }
 
-export const commands = [PasteCommand, XPCommand, RoleCommand, SetCommand]
+export const commands = [PasteCommand, XPCommand, RoleCommand, SetCommand, InfoCommand]
---
+++ src/commands/InfoCommand.ts
@@ -0,0 +1,72 @@
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {CommandInteraction, GuildMember} from "discord.js";
+import {Command} from "./Commands.js";
+import {createStandardEmbed} from "../util/embeds.js";
+import {DDUser} from "../store/models/DDUser.js";
+import {SavedMessage} from "../store/models/SavedMessage.js";
+
+export class InfoCommand implements Command {
+    info = new SlashCommandBuilder()
+        .setName("info")
+        .setDescription("Show information about the bot and server")
+
+
+    async execute(interaction: CommandInteraction) {
+        await interaction.deferReply()
+        const guild = interaction.guild!!;
+        const allUsers = await DDUser.findAll();
+        const totalXP = allUsers.reduce((acc, user) => acc + user.xp, 0)
+        const memberCount = guild.memberCount;
+        const membersStored = allUsers.length;
+        const dateCreated = `
+            <t:${guild.createdAt.getTime() / 1000 | 0}>`;
+        const levelUps = allUsers.reduce((acc, user) => acc + user.level, 0);
+        const messageCount = (await SavedMessage.findAll()).length;
+        await interaction.followUp({
+            embeds: [{
+                ...createStandardEmbed(interaction.member as GuildMember),
+                title: "Developer Den",
+                description: "This is the bot for the Developer Den server. It's written in **TypeScript** using the **Discord.js** library." +
+                    "The source can be found [here](https://github.com/TheDeveloperDen/DevDenBot)",
+                fields: [
+                    {
+                        name: "Version",
+                        value: format(process.env.npm_package_version), inline: true
+                    },
+                    {
+                        name: "Total XP",
+                        value: format(totalXP), inline: true
+                    },
+                    {
+                        name: "Member Count",
+                        value: format(memberCount), inline: true
+                    },
+                    {
+                        name: "Members Stored",
+                        value: format(membersStored), inline: true
+                    },
+                    {
+                        name: "Level Ups",
+                        value: format(levelUps), inline: true
+                    },
+                    {
+                        name: "Messages Stored",
+                        value: format(messageCount), inline: true
+                    },
+                    {
+                        name: "Date Created",
+                        value: dateCreated, inline: true
+                    }
+                ]
+            }]
+        })
+    }
+}
+
+const format = (val: any) => {
+    if (typeof val === "number") {
+        return `\`${val.toLocaleString()}\``
+    } else {
+        return `\`${val}\``
+    }
+}
