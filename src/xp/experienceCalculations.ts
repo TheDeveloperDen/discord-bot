24ea34985665f3f637bfe9f2b7af28429ab15dbd
Redempt <instantiable@gmail.com>
2022-02-11 18:44:24 -0500

    Factor compressibility into experience calculations

--- src/xp/experienceCalculations.ts
+++ src/xp/experienceCalculations.ts
@@ -11,7 +11,32 @@ const strip = compose(stripPunctuation, stripPings)
 
 export const xpForLevel = (level: number) => Math.floor(level ** 3 + 27 * level ** 2 + 125 * level)
 
+function findForward(input: string, index: number, set: Set<string>): number {
+    let current = ""
+    while (set.has(current) && index < input.length) {
+        current = current.concat(input[index])
+        index++
+    }
+    set.add(current)
+    console.log(current)
+    return current.length
+}
+
+function compressibility(input: string): number {
+    input = input.toLowerCase()
+    let things = new Set<string>()
+    things.add("")
+    let cut = 0
+    let i = 0
+    while (i < input.length) {
+        let length = Math.max(findForward(input, i, things) - 1, 0)
+        cut += length
+        i += length + 1
+    }
+    return cut / input.length
+}
+
 export const xpForMessage = (message: string) => {
-	const length = strip(message).length * 0.8
-	return Math.round(Math.tanh(length / 3) * Math.pow(length, 0.75))
+	const length = strip(message).length
+	return Math.round((1 - compressibility(message)) * Math.tanh(length / 3) + Math.pow(length, 0.75))
 }
