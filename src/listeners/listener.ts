342052f1f9bd95dce4b1acc6239eb53c539b8132
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-05-31 12:21:31 +0100

    Completely remove bump notifications :(

--- src/listeners/listener.ts
+++ src/listeners/listener.ts
@@ -2,7 +2,6 @@ import {MarkedClient} from '../MarkedClient.js'
 import {messageLoggerListener} from './messageLogger.js'
 import {languageStatusListener} from './languageStatus.js'
 import {joinLeaveListener} from './joinLeaveMessages.js'
-import {bumpNotificationListener} from './bumpNotifications.js'
 import xpHandler from '../xp/xpHandler.js'
 import {showcaseListener} from './showcase.js'
 import {roleChangeListener} from '../xp/roleUpdates.js'
@@ -17,7 +16,6 @@ export type Listener = (client: MarkedClient) => void;
 export const listeners = [xpHandler,
 	messageLoggerListener,
 	roleChangeListener,
-	bumpNotificationListener,
 	joinLeaveListener,
 	languageStatusListener,
 	pastebinListener,
