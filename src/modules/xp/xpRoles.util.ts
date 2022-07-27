47b49b7f26771fb3dc6e061513e7e77ec9367a68
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-07-27 21:01:33 +0100

    Always mention in the Level up embed since it wont ping anyway

--- src/modules/xp/xpRoles.util.ts
+++ src/modules/xp/xpRoles.util.ts
@@ -3,7 +3,7 @@ import {DDUser} from '../../store/models/DDUser.js'
 import {modifyRoles} from '../../util/roles.js'
 import {config} from '../../Config.js'
 import {createStandardEmbed} from '../../util/embeds.js'
-import {mention, mentionWithNoPingMessage, pseudoMention} from '../../util/users.js'
+import {mention, mentionWithNoPingMessage} from '../../util/users.js'
 import {tierRoleId, xpForLevel} from './xpForMessage.util.js'
 import {logger} from '../../logging.js'
 
@@ -42,7 +42,7 @@ async function sendLevelUpMessage(client: Client, member: GuildMember, ddUser: D
 		...createStandardEmbed(member),
 		title: '⚡ Level Up!',
 		author: {
-			name: pseudoMention(user),
+			name: mention(user),
 			iconURL: user.avatarURL()
 		},
 		fields: [
