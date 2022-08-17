963d627dc3743cc53fcadddc53329f476cc277b2
Lucy Poulton <lucy@lucypoulton.net>
2022-08-17 21:05:08 +0100

    delete colour role when someone stops boosting

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,8 +1,10 @@
 import Module from '../module.js'
 import {RoleCommand} from './role.command.js'
 import {RoleColourCommand} from './roleColour.command.js'
+import {RoleColourListener} from './roleColour.listener.js'
 
 export const RolesModule: Module = {
 	name: 'roles',
-	commands: [RoleCommand, RoleColourCommand]
+	commands: [RoleCommand, RoleColourCommand],
+	listeners: [RoleColourListener]
 }
