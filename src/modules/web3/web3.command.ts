b2b4884623802607935cbe1d48556810868e215e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-09-08 20:22:43 +0200

    Use manual fetch request to get RSS feed, work around turndown preserving img tags, make embed prettier

--- src/modules/web3/web3.command.ts
+++ src/modules/web3/web3.command.ts
@@ -3,6 +3,7 @@ import {ApplicationCommandType} from 'discord-api-types/v10'
 import {CommandInteraction, EmbedBuilder} from 'discord.js'
 import Parser from 'rss-parser'
 import TurndownService from 'turndown'
+import fetch from 'node-fetch'
 
 
 const web3RssUrl = 'https://web3isgoinggreat.com/feed.xml'
@@ -11,6 +12,8 @@ const rssParser = new Parser()
 
 const turndownService = new TurndownService({ bulletListMarker: '-' })
 
+const imgTag = /<img.*?\/>/g
+
 export const Web3Command: Command<ApplicationCommandType.ChatInput> = {
 	name: 'web3fact',
 	description: 'Learn more about Web3, the future of human society',
@@ -18,15 +21,24 @@ export const Web3Command: Command<ApplicationCommandType.ChatInput> = {
 	options: [],
 
 	async handle(interaction: CommandInteraction) {
-		const feed = await rssParser.parseURL(web3RssUrl)
+		const xml = await fetch(web3RssUrl, {compress: true})
+			.then(r => r.text())
+			.catch(e => console.log(e))
+		if (xml == undefined) {
+			await interaction.reply({content: 'Couldn\'t fetch RSS feed.'})
+			return
+		}
+		const feed = await rssParser.parseString(xml as string)
 		const recentEntries = feed.items.slice(0, 10)
 		const entry = recentEntries.randomElement()
 		const embed = new EmbedBuilder()
 			.setTitle(entry.title ?? 'Missing title')
-			.setAuthor({name: 'Molly White', url: 'https://mollywhite.net'})
+			.setAuthor({name: 'Molly White', url: 'https://mollywhite.net', iconURL: 'https://storage.googleapis.com/primary-web3/monkey_500.webp'})
+			.setColor(0x1F193A)
 			.setURL(entry.link ?? feed.link ?? 'http://example.com')
-			.setDescription(turndownService.turndown(entry.content ?? 'Missing content'))
-			.setTimestamp(new Date(entry.pubDate ?? Date().toString()))
+			// why do I have to regex match img tags? because turndown filters don't detect them for some reason
+			.setDescription(turndownService.turndown(entry.content?.replaceAll(imgTag, '') ?? 'Missing content'))
+			.setTimestamp(new Date(entry.isoDate ?? Date().toString()))
 			.setFooter({text: feed.title ?? 'Missing feed title'})
 			.toJSON()
 		await interaction.reply({embeds: [embed]})
