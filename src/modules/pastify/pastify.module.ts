7de811b91e905cd5545336a4f72ccf7e9f42f6a0
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-01-01 20:45:40 +0000

    make pastification ignore bots and staff

--- src/modules/pastify/pastify.module.ts
+++ src/modules/pastify/pastify.module.ts
@@ -2,19 +2,26 @@ import Module from '../module.js'
 import {pastify} from './pastify.js'
 import {PastifyCommand} from './pastify.command.js'
 import {PasteCommand} from './paste.command.js'
+import {PermissionFlagsBits} from "discord-api-types/v10";
 
 export const PastifyModule: Module = {
-	name: 'pastify',
-	commands: [PastifyCommand, PasteCommand],
-	listeners: [{
-		async messageCreate(_, message) {
-			if (message.channel.isThread()) {
-				return // Don't pastify messages in threads since theres nothing to "interrupt"
-			}
-			const pastified = await pastify(message)
-			if (pastified) return message.channel.send({...pastified, flags: 0})
-		}
-	}]
+    name: 'pastify',
+    commands: [PastifyCommand, PasteCommand],
+    listeners: [{
+        async messageCreate(_, message) {
+            if (message.channel.isThread()) {
+                return // Don't pastify messages in threads since theres nothing to "interrupt"
+            }
+            if (message.author.bot) {
+                return // Don't pastify messages from bots, it makes things like /run behave weirdly
+            }
+            if (message.member?.permissions.has(PermissionFlagsBits.ManageMessages)) {
+                return // Don't pastify messages from staff as they should know better
+            }
+            const pastified = await pastify(message)
+            if (pastified) return message.channel.send({...pastified, flags: 0})
+        }
+    }]
 }
 
 export default PastifyModule
