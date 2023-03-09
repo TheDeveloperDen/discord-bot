6ff2df5fdb5977a7152c868fbb63bcc9ea67115b
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-03-09 09:10:19 +0000

    dont allow reaction to own showcase posts, or adding both reactions

--- src/modules/showcase.module.ts
+++ src/modules/showcase.module.ts
@@ -9,6 +9,34 @@ export const ShowcaseModule: Module = {
 			if (message.author.bot || message.system) return
 			await message.react('👍')
 			await message.react('👎')
+		},
+
+		async messageReactionAdd(_, reaction, user) {
+			if (reaction.message.channel.id !== config.channels.showcase) return
+			if (user.bot || user.system) return
+			if (user.partial) {
+				user = await user.fetch()
+			}
+			if (reaction.partial) {
+				reaction = await reaction.fetch()
+			}
+			if (user.id === reaction.message.author?.id) {
+				await reaction.users.remove(user)
+				return
+			}
+
+			const userHasReacted = async (emoji: string) => {
+				const rs = await reaction.message.awaitReactions({filter: r => r.emoji.name === emoji})
+				return rs.filter(r => r.users.resolve(user.id) != null)
+			}
+			if (reaction.emoji.name === '👍' && await userHasReacted('👎')) {
+				await reaction.users.remove(user)
+				return
+			}
+			if (reaction.emoji.name === '👎' && await userHasReacted('👍')) {
+				await reaction.users.remove(user)
+				return
+			}
 		}
 	}]
 }
