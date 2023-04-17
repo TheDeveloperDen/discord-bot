2bef9a5608a7b272fe24e92ab749a631b17d8cca
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-04-17 22:04:21 +0100

    optimise /daily a bit more

--- src/modules/xp/dailyReward.command.ts
+++ src/modules/xp/dailyReward.command.ts
@@ -7,62 +7,67 @@ import {createStandardEmbed} from '../../util/embeds.js'
 import {giveXp} from './xpForMessage.util.js'
 
 export const DailyRewardCommand: Command<ApplicationCommandType.ChatInput> = {
-	name: 'daily',
-	description: 'Claim your daily reward',
-	type: ApplicationCommandType.ChatInput,
-	options: [],
+    name: 'daily',
+    description: 'Claim your daily reward',
+    type: ApplicationCommandType.ChatInput,
+    options: [],
 
-	async handle(interaction: CommandInteraction) {
-		const user = interaction.member as GuildMember
-		if (!user) {
-			await interaction.reply('You must be in a guild to use this command')
-			return
-		}
-		await interaction.deferReply()
-		const ddUser = await getUserById(BigInt(user.id))
-		const difference = new Date().getTime() - (ddUser.lastDailyTime?.getTime() ?? 0)
-		if (difference < 1000 * 60 * 60 * 24) {
-			const lastClaimTime = ddUser.lastDailyTime
-			if (!lastClaimTime) {
-				logger.error('lastClaimTime is null')
-				return
-			}
-			const nextClaimTime = new Date(lastClaimTime.getTime() + 1000 * 60 * 60 * 24)
-			await interaction.followUp({
-				ephemeral: true,
-				content: `You can only claim your daily reward once every 24 hours. You can claim it again <t:${nextClaimTime.getTime() / 1000}:R>.`
-			})
-			return
-		}
+    async handle(interaction: CommandInteraction) {
+        const user = interaction.member as GuildMember
+        if (!user) {
+            await interaction.reply('You must be in a guild to use this command')
+            return
+        }
+        await interaction.deferReply()
+        const ddUser = await getUserById(BigInt(user.id))
+        const difference = new Date().getTime() - (ddUser.lastDailyTime?.getTime() ?? 0)
+        if (difference < 1000 * 60 * 60 * 24) {
+            const lastClaimTime = ddUser.lastDailyTime
+            if (!lastClaimTime) {
+                logger.error('lastClaimTime is null')
+                return
+            }
+            const nextClaimTime = new Date(lastClaimTime.getTime() + 1000 * 60 * 60 * 24)
+            await interaction.followUp({
+                ephemeral: true,
+                content: `You can only claim your daily reward once every 24 hours. You can claim it again <t:${nextClaimTime.getTime() / 1000}:R>.`
+            })
+            return
+        }
 
-		ddUser.currentDailyStreak = await getActualDailyStreak(ddUser) + 1
-		if (ddUser.currentDailyStreak > ddUser.highestDailyStreak) {
-			ddUser.highestDailyStreak = ddUser.currentDailyStreak
-		}
+        const [_, streak] = getActualDailyStreakWithoutSaving(ddUser)
+        ddUser.currentDailyStreak = streak + 1
 
-		const xpToGive = Math.min(50 + 20 * (ddUser.currentDailyStreak - 1), 1000)
-		const {xpGiven, multiplier} = await giveXp(user, xpToGive)
-		ddUser.lastDailyTime = new Date()
-		await ddUser.save()
+        if (ddUser.currentDailyStreak > ddUser.highestDailyStreak) {
+            ddUser.highestDailyStreak = ddUser.currentDailyStreak
+        }
 
-		await interaction.followUp({
-			ephemeral: false,
-			embeds: [
-				createStandardEmbed(user)
-					.setTitle('Daily Reward Claimed!')
-					.setDescription(`📆 Current Streak = ${formatDayCount(ddUser.currentDailyStreak)}
+        const xpToGive = Math.min(50 + 20 * (ddUser.currentDailyStreak - 1), 1000)
+        const {xpGiven, multiplier} = await giveXp(user, xpToGive)
+        ddUser.lastDailyTime = new Date()
+        await Promise.all(
+            [
+                interaction.followUp({
+                    ephemeral: false,
+                    embeds: [
+                        createStandardEmbed(user)
+                            .setTitle('Daily Reward Claimed!')
+                            .setDescription(`📆 Current Streak = ${formatDayCount(ddUser.currentDailyStreak)}
 ⭐️ + ${xpGiven} XP  ${multiplier ? `(x${multiplier})` : ''}
 ⏰ Come back in 24 hours for a new reward!`)
-			]
-		})
-	}
+                    ]
+                })
+                , ddUser.save()
+            ]
+        )
+    }
 }
 
 export function formatDayCount(count: number) {
-	if (count === 1) {
-		return '1 day'
-	}
-	return `${count} days`
+    if (count === 1) {
+        return '1 day'
+    }
+    return `${count} days`
 }
 
 
@@ -71,12 +76,25 @@ export function formatDayCount(count: number) {
  * @param ddUser The user to get the streak for
  */
 export async function getActualDailyStreak(ddUser: DDUser): Promise<number> {
-	const difference = new Date().getTime() - (ddUser.lastDailyTime?.getTime() ?? 0)
-	if (difference >= 1000 * 60 * 60 * 24 * 2) {
-		// Set streak to 0
-		ddUser.currentDailyStreak = 0
-		await ddUser.save()
-	}
+    const [reset, streak] = getActualDailyStreakWithoutSaving(ddUser)
+    if (reset) {
+        await ddUser.save()
+    }
+    return streak
+}
 
-	return ddUser.currentDailyStreak
+/**
+ * Gets a user's current daily streak, resetting it to 0 if they haven't used it in 24 hours.
+ * This function will not save the model
+ * @param ddUser The user to get the streak for
+ */
+export function getActualDailyStreakWithoutSaving(ddUser: DDUser): [boolean, number] {
+    const difference = new Date().getTime() - (ddUser.lastDailyTime?.getTime() ?? 0)
+    if (difference >= 1000 * 60 * 60 * 24 * 2) {
+        // Set streak to 0
+        ddUser.currentDailyStreak = 0
+        return [true, ddUser.currentDailyStreak]
+    }
+
+    return [false, ddUser.currentDailyStreak]
 }
