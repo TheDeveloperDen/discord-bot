5b9514457d00ad851fdf2724f975ce99df2bd425
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-04-17 21:51:05 +0100

    more logging #2

--- src/store/storage.ts
+++ src/store/storage.ts
@@ -10,28 +10,39 @@ const password = process.env.PASSWORD ?? 'password'
 const host = process.env.HOST ?? 'localhost'
 
 export const sequelize = new Sequelize({
-	database: database,
-	username: username,
-	password: password,
-	models: [],
-	host: host,
-	dialect: 'mariadb',
-	logging: (msg) => logger.debug(msg),
-	logQueryParameters: true,
-	benchmark: true
+    database: database,
+    username: username,
+    password: password,
+    models: [],
+    host: host,
+    dialect: 'mariadb',
+    logging: sequelizeLog,
+    logQueryParameters: true,
+    benchmark: true
 })
+
+function sequelizeLog(sql: string, timing?: number) {
+    if (timing) {
+        if (timing >= 10) {
+            logger.warning(`Slow query (${timing}ms): ${sql}`)
+        }
+    } else {
+        logger.debug(sql)
+    }
+}
+
 
 let resolve: () => void
 export const databaseInit = new Promise<void>(r => resolve = r)
 
 export async function init() {
-	const models = [DDUser, ColourRoles, FAQ]
-	sequelize.addModels(models)
-	for (const model of models) {
-		await model.sync()
-	}
-	resolve()
-	logger.info('Initialised database')
+    const models = [DDUser, ColourRoles, FAQ]
+    sequelize.addModels(models)
+    for (const model of models) {
+        await model.sync()
+    }
+    resolve()
+    logger.info('Initialised database')
 }
 
 await init()
