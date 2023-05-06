95c0993637b7eb3c197555e8a83498670024de84
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-06 16:53:50 +0100

    make database optional

--- src/store/storage.ts
+++ src/store/storage.ts
@@ -3,23 +3,33 @@ import {logger} from '../logging.js'
 import {DDUser} from './models/DDUser.js'
 import {ColourRoles} from './models/ColourRoles.js'
 import {FAQ} from './models/FAQ.js'
+import {Dialect} from 'sequelize/types'
 
 const database = process.env.DATABASE ?? 'database'
-const username = process.env.USERNAME ?? 'admin'
+const username = process.env.USERNAME ?? 'root'
 const password = process.env.PASSWORD ?? 'password'
 const host = process.env.HOST ?? 'localhost'
+const port = process.env.PORT ?? '3306'
+const dialect = process.env.DIALECT ?? 'mariadb'
 
-export const sequelize = new Sequelize({
-	database: database,
-	username: username,
-	password: password,
-	models: [],
-	host: host,
-	dialect: 'mariadb',
+const commonSequelizeSettings = {
 	logging: sequelizeLog,
 	logQueryParameters: true,
-	benchmark: true
-})
+	benchmark: true,
+	models: []
+}
+
+export const sequelize = process.env.HOST ? new Sequelize({
+		database: database,
+		username: username,
+		password: password,
+		host: host,
+		port: parseInt(port),
+		dialect: dialect as Dialect,
+		...commonSequelizeSettings
+	})
+	: new Sequelize("sqlite::memory:", commonSequelizeSettings)
+
 
 function sequelizeLog(sql: string, timing?: number) {
 	if (timing) {
