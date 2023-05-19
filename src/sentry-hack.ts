13d899f8861f80450b793ff3f4ead264b6cda64c
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-19 18:49:43 +0100

    fix sentry hack

--- src/sentry-hack.ts
+++ src/sentry-hack.ts
@@ -2,7 +2,7 @@
 // noinspection JSUnusedGlobalSymbols,ES6ConvertVarToLetConst
 
 // This allows TypeScript to detect our global value
-global.__rootdir__ = __dirname || process.cwd()
+global.__rootdir__ = process.cwd()
 
 declare global {
   var __rootdir__: string
