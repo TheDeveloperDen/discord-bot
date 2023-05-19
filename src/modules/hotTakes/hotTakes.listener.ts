347bfeb5d88839568edcf9889832416d00ce5345
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-19 15:19:00 +0100

    hot take debugging

--- src/modules/hotTakes/hotTakes.listener.ts
+++ src/modules/hotTakes/hotTakes.listener.ts
@@ -3,17 +3,22 @@ import { config } from '../../Config.js'
 import { EventListener } from '../module.js'
 import generateHotTake from './hotTakes.util.js'
 import { awaitTimeout } from '../../util/timeouts.js'
+import { logger } from '../../logging.js'
 
 async function sendHotTake (client: Client) {
   const channel = await client.channels.fetch(
-    config.channels.hotTake) as TextChannel
+    config.channels.hotTake
+  ) as TextChannel
   const lastMessage = await channel.messages.fetch({ limit: 1 })
-    .then(m => m.first())
+    .then((m) => m.first())
   const lastMessageSentAt = lastMessage?.createdAt ?? new Date(0)
+
+  // time since last message in seconds
   const timeSinceLastMessage = (Date.now() - lastMessageSentAt.getTime()) / 1000
   if (lastMessage?.author?.bot ?? timeSinceLastMessage < 60 * 60 * 2) {
     return
   }
+  logger.debug(`Time since last message: ${timeSinceLastMessage}, met threshold`)
   const hotTake = await generateHotTake(channel.guild)
   await channel.send({
     content: hotTake,
