6b83dfb0077eaedc663fc05c8d38ea18dd5a3ca5
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-05-24 22:20:04 +0100

    let me cook

--- src/modules/faq/faq.module.ts
+++ src/modules/faq/faq.module.ts
@@ -1,12 +1,16 @@
 import Module from '../module.js'
 import { FaqCommandListener } from './faqCommand.listener.js'
 import { FaqCommand, updateChoices } from './faq.command.js'
+import { moduleManager } from '../../index.js'
 
 export const FaqModule: Module = {
   name: 'faq',
   commands: [FaqCommand],
   listeners: [FaqCommandListener],
-  onCommandInit: updateChoices
+  onCommandInit: updateChoices,
+  onInit: async () => {
+    await moduleManager.refreshCommands()
+  }
 }
 
 export default FaqModule
