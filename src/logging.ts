7912e3f9a9b47bce710c6a0aee2ba4e265b7d759
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-10-25 13:32:49 -0400

    add timestamps to the logs!

--- src/logging.ts
+++ src/logging.ts
@@ -1,36 +1,46 @@
-import { createLogger, format, transports } from 'winston'
+import {createLogger, format, transports} from 'winston'
 
 const timestamp = format.timestamp({
-  format: 'YYYY-MM-DD HH:mm:ss'
+    format: 'YYYY-MM-DD HH:mm:ss'
 })
 const baseFormat = format.combine(
-  timestamp,
-  format.errors({ stack: true }),
-  format.splat(),
-  format.json()
+    timestamp,
+    format.errors({stack: true}),
+    format.splat(),
+    format.json()
 )
 
-export const logger = createLogger({
-  level: 'info',
-  format: baseFormat,
-  defaultMeta: { service: 'DevDenBot' },
-  transports: [
-    // something slightly more readable for the cli
-    new transports.Console({ format: format.combine(timestamp, format.cli()) }),
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new transports.File({
-      filename: 'logs/error.log',
-      level: 'error'
+const cliFormat = format.combine(
+    baseFormat,
+    format.colorize({
+        level: true,
     }),
-    // write all logs to a file named the current time
-    new transports.File({
-      filename: `logs/${new Date().toISOString().replace(/:/g, '-')}.log`,
-      level: 'info'
-    })
-  ]
+    format.printf(
+        (info) => `[${info.timestamp}] ${info.level}: ${info.message}`
+    )
+);
+
+export const logger = createLogger({
+    level: 'info',
+    format: baseFormat,
+    defaultMeta: {service: 'DevDenBot'},
+    transports: [
+        // something slightly more readable for the cli
+        new transports.Console({format: format.combine(format.cli(), cliFormat)}),
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        new transports.File({
+            filename: 'logs/error.log',
+            level: 'error'
+        }),
+        // write all logs to a file named the current time
+        new transports.File({
+            filename: `logs/${new Date().toISOString().replace(/:/g, '-')}.log`,
+            level: 'info'
+        })
+    ]
 })
 
 //
@@ -38,5 +48,5 @@ export const logger = createLogger({
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.NODE_ENV !== 'production') {
-  logger.level = 'debug'
+    logger.level = 'debug'
 }
