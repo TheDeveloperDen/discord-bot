27ac0e5c83055a3038d8d401a369ed2ace230518
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-10-25 15:10:38 -0400

    perhaps this will do something

--- instrument.mjs
+++ instrument.mjs
@@ -6,7 +6,8 @@ Sentry.init({
     dsn: process.env.DDB_SENTRY_DSN,
 
     release: process.env.npm_package_version ?? process.env.VERSION ?? 'unknown',
-    tracesSampleRate: 0.2,
+    debug:true,
+    tracesSampleRate: 1.0,
     profilesSampleRate: 0.2,
     integrations: [
         nodeProfilingIntegration(),
