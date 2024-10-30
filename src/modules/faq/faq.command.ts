2e84ac9e300773d84109c8871a6e75db1e2362ad
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-10-29 20:48:49 -0400

    extremely tentative removal of json.ts

--- src/modules/faq/faq.command.ts
+++ src/modules/faq/faq.command.ts
@@ -1,157 +1,156 @@
-import { Command, ExecutableSubcommand } from 'djs-slash-helper'
-import { ApplicationCommandOptionType, ApplicationCommandType, PermissionFlagsBits } from 'discord-api-types/v10'
-import { FAQ } from '../../store/models/FAQ.js'
-import { createFaqEmbed } from './faq.util.js'
+import {Command, ExecutableSubcommand} from 'djs-slash-helper'
+import {ApplicationCommandOptionType, ApplicationCommandType, PermissionFlagsBits} from 'discord-api-types/v10'
+import {FAQ} from '../../store/models/FAQ.js'
+import {createFaqEmbed} from './faq.util.js'
 import createFaqModal from './faq.modal.js'
-import { moduleManager } from '../../index.js'
-import { logger } from '../../logging.js'
-import { toJson } from '../../json.js'
-import { GuildMember } from 'discord.js'
+import {moduleManager} from '../../index.js'
+import {logger} from '../../logging.js'
+import {GuildMember} from 'discord.js'
 
 const choices: Array<{ name: string, value: string }> = []
 
-export async function updateChoices () {
-  const result = await FAQ.findAll()
-  choices.length = 0
-  choices.push(...result.map((it) => ({
-    name: it.name,
-    value: it.name
-  })))
-  logger.info(`Updated FAQ choices to ${toJson(choices)}`)
+export async function updateChoices() {
+    const result = await FAQ.findAll()
+    choices.length = 0
+    choices.push(...result.map((it) => ({
+        name: it.name,
+        value: it.name
+    })))
+    logger.info(`Updated FAQ choices to ${JSON.stringify(choices)}`)
 }
 
 const GetSubcommand: ExecutableSubcommand = {
-  type: ApplicationCommandOptionType.Subcommand,
-  name: 'get',
-  description: 'Get a FAQ entry\'s content',
-  options: [
-    {
-      type: ApplicationCommandOptionType.String,
-      name: 'name',
-      description: 'The name of the FAQ',
-      required: true,
-      choices
+    type: ApplicationCommandOptionType.Subcommand,
+    name: 'get',
+    description: 'Get a FAQ entry\'s content',
+    options: [
+        {
+            type: ApplicationCommandOptionType.String,
+            name: 'name',
+            description: 'The name of the FAQ',
+            required: true,
+            choices
+        }
+    ],
+    async handle(interaction) {
+        const name = interaction.options.get('name')?.value as string | null
+        const faq = await FAQ.findOne({
+            where: {name: name ?? undefined}
+        })
+        if (faq == null) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No FAQ found with this name'
+            })
+        }
+        return await interaction.reply({embeds: [createFaqEmbed(faq)]})
     }
-  ],
-  async handle (interaction) {
-    const name = interaction.options.get('name')?.value as string | null
-    const faq = await FAQ.findOne({
-      where: { name: name ?? undefined }
-    })
-    if (faq == null) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No FAQ found with this name'
-      })
-    }
-    return await interaction.reply({ embeds: [createFaqEmbed(faq)] })
-  }
 }
 
 const EditSubcommand: ExecutableSubcommand = {
-  type: ApplicationCommandOptionType.Subcommand,
-  name: 'edit',
-  description: 'Edit a FAQ entry, or create a new one if it doesn\'t exist',
-  options: [
-    {
-      type: ApplicationCommandOptionType.String,
-      name: 'name',
-      description: 'The name of the FAQ',
-      required: true
+    type: ApplicationCommandOptionType.Subcommand,
+    name: 'edit',
+    description: 'Edit a FAQ entry, or create a new one if it doesn\'t exist',
+    options: [
+        {
+            type: ApplicationCommandOptionType.String,
+            name: 'name',
+            description: 'The name of the FAQ',
+            required: true
+        }
+    ],
+    async handle(interaction) {
+        const member = interaction.member as GuildMember
+        if (!member.permissions.has(PermissionFlagsBits.ManageMessages)) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No permission'
+            })
+        }
+        const name = interaction.options.get('name')?.value as string | null
+        if (name == null) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No FAQ name provided'
+            })
+        }
+        const faq = await FAQ.findOne({where: {name}})
+
+        const modal = createFaqModal(faq ?? undefined)
+
+        await interaction.showModal(modal)
+        const response = await interaction.awaitModalSubmit({time: 2 ** 31 - 1})
+        const title = response.fields.getTextInputValue('titleField')
+        const content = response.fields.getTextInputValue('faqContentField')
+
+        await FAQ.upsert({
+            id: faq?.id,
+            name,
+            title,
+            content,
+            author: BigInt(interaction.user.id)
+        })
+        await response.reply({
+            ephemeral: true,
+            content: `FAQ named ${name} created`
+        })
+
+        await updateChoices()
+        return await moduleManager.refreshCommands()
     }
-  ],
-  async handle (interaction) {
-    const member = interaction.member as GuildMember
-    if (!member.permissions.has(PermissionFlagsBits.ManageMessages)) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No permission'
-      })
-    }
-    const name = interaction.options.get('name')?.value as string | null
-    if (name == null) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No FAQ name provided'
-      })
-    }
-    const faq = await FAQ.findOne({ where: { name } })
-
-    const modal = createFaqModal(faq ?? undefined)
-
-    await interaction.showModal(modal)
-    const response = await interaction.awaitModalSubmit({ time: 2 ** 31 - 1 })
-    const title = response.fields.getTextInputValue('titleField')
-    const content = response.fields.getTextInputValue('faqContentField')
-
-    await FAQ.upsert({
-      id: faq?.id,
-      name,
-      title,
-      content,
-      author: BigInt(interaction.user.id)
-    })
-    await response.reply({
-      ephemeral: true,
-      content: `FAQ named ${name} created`
-    })
-
-    await updateChoices()
-    return await moduleManager.refreshCommands()
-  }
 }
 
 const DeleteSubcommand: ExecutableSubcommand = {
-  type: ApplicationCommandOptionType.Subcommand,
-  name: 'delete',
-  description: 'Delete a FAQ entry',
-  options: [
-    {
-      type: ApplicationCommandOptionType.String,
-      name: 'name',
-      description: 'The name of the FAQ',
-      required: true,
-      choices
-    }
-  ],
-  async handle (interaction) {
-    const member = interaction.member as GuildMember
-    if (!member.permissions.has(PermissionFlagsBits.ManageMessages)) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No permission'
-      })
-    }
-    const name = interaction.options.get('name')?.value as string | null
-    if (name == null) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No FAQ name provided'
-      })
-    }
+    type: ApplicationCommandOptionType.Subcommand,
+    name: 'delete',
+    description: 'Delete a FAQ entry',
+    options: [
+        {
+            type: ApplicationCommandOptionType.String,
+            name: 'name',
+            description: 'The name of the FAQ',
+            required: true,
+            choices
+        }
+    ],
+    async handle(interaction) {
+        const member = interaction.member as GuildMember
+        if (!member.permissions.has(PermissionFlagsBits.ManageMessages)) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No permission'
+            })
+        }
+        const name = interaction.options.get('name')?.value as string | null
+        if (name == null) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No FAQ name provided'
+            })
+        }
 
-    const faq = await FAQ.findOne({ where: { name } })
-    if (faq == null) {
-      return await interaction.reply({
-        ephemeral: true,
-        content: 'No FAQ found with this name'
-      })
+        const faq = await FAQ.findOne({where: {name}})
+        if (faq == null) {
+            return await interaction.reply({
+                ephemeral: true,
+                content: 'No FAQ found with this name'
+            })
+        }
+        await faq.destroy()
+        await updateChoices()
+        await moduleManager.refreshCommands()
+        return await interaction.reply({
+            ephemeral: true,
+            content: `FAQ named ${name} deleted`
+        })
     }
-    await faq.destroy()
-    await updateChoices()
-    await moduleManager.refreshCommands()
-    return await interaction.reply({
-      ephemeral: true,
-      content: `FAQ named ${name} deleted`
-    })
-  }
 }
 
 export const FaqCommand: Command<ApplicationCommandType.ChatInput> = {
-  name: 'faq',
-  description: 'Get / set FAQs',
-  type: ApplicationCommandType.ChatInput,
-  options: [GetSubcommand, EditSubcommand, DeleteSubcommand],
-  handle () {
-  }
+    name: 'faq',
+    description: 'Get / set FAQs',
+    type: ApplicationCommandType.ChatInput,
+    options: [GetSubcommand, EditSubcommand, DeleteSubcommand],
+    handle() {
+    }
 }
