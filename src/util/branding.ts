13fe23647f8f8fc4547831082fa25573b9098735
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-01-31 21:20:14 -0500

    oh

--- src/util/branding.ts
+++ src/util/branding.ts
@@ -11,21 +11,16 @@ export interface BrandingConfig {
     color: string
 }
 
-export let branding: Required<BrandingConfig> = {
+export const branding: Required<BrandingConfig> = {
     name: 'Developer Den',
-    iconUrl: 'https://developerden.org/logo.png',
+    iconUrl: 'https://static.developerden.org/logo.png',
     ...config.branding
 }
 
 export async function setupBranding(guild: Guild) {
     guild = await guild.fetch()
     logger.debug(`Setting up branding with guild ${guild.name} and ${guild.iconURL()}`)
-    branding = {
-        ...{
-            name: guild.name ?? branding.name,
-            iconUrl: guild.iconURL() ?? branding.iconUrl
-        },
-        ...config.branding
-    }
+    branding.name = guild.name
+    branding.iconUrl = guild.iconURL() ?? branding.iconUrl
     logger.debug(`Branding config set to ${JSON.stringify(branding)}`)
 }
