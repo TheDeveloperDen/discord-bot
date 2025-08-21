ef0a95067686c62b2f19ac2c766bfc9ddf3c49ab
Pdzly <rooki@rooki.xyz>
2025-08-21 12:26:12 +0200

    Introduce `DiscordInvitesMonitorModule` to manage invite links and enforce tier-based restrictions.

---
+++ src/modules/discordInvitesMonitor.module.ts
@@ -0,0 +1,136 @@
+import Module from "./module.js";
+import { actualMention, isSpecialUser } from "../util/users.js";
+import { config } from "../Config.js";
+import { getMember } from "../util/member.js";
+import { GuildMember } from "discord.js";
+import { getOrCreateUserById } from "../store/models/DDUser.js";
+import { getTierByLevel } from "./xp/xpForMessage.util.js";
+
+const invitePatterns = [
+  /discord\.gg\/[a-zA-Z0-9]+/gi,
+  /discordapp\.com\/invite\/[a-zA-Z0-9]+/gi,
+  /discord\.com\/invite\/[a-zA-Z0-9]+/gi,
+  /(^| )\.gg\/[a-zA-Z0-9]+/gi,
+];
+
+const whitelistDomains: string[] = []; // For any .gg domains that are not discord.gg
+
+const isAllowedToSendDiscordInvites = async (member: GuildMember) => {
+  const ddUser = await getOrCreateUserById(BigInt(member.id));
+  return getTierByLevel(ddUser.level) >= 2;
+};
+export const DiscordInvitesMonitorModule: Module = {
+  name: "discordInvitesMonitor",
+  listeners: [
+    {
+      async messageCreate(_, message) {
+        if (message.author.bot || !message.inGuild()) return;
+        const member = await getMember(message);
+        if (!member || isSpecialUser(member)) return;
+        if (await isAllowedToSendDiscordInvites(member)) return;
+
+        // Check if message contains any Discord invite
+        const matches = invitePatterns
+          .map((pattern) => message.content.match(pattern))
+          .filter((match) => match != null && match.length > 0)
+          .map((match) => match![0])
+          .filter(
+            (match) =>
+              !whitelistDomains.some((domain) => match.includes(domain)),
+          );
+
+        const hasInvite = matches.length > 0;
+        console.log(hasInvite, matches);
+
+        if (hasInvite) {
+          try {
+            await message.delete();
+
+            const warningMessage = await message.channel.send({
+              content: `${actualMention(member)}, only Users with Tier 2 or over are allowed to send Discord invites.\nPlease remove the invite before sending it again.\nThank you!`,
+            });
+
+            setTimeout(() => {
+              warningMessage.delete().catch(() => {});
+            }, 10000);
+
+            const auditChannel = await message.guild.channels.fetch(
+              config.channels.auditLog,
+            );
+
+            if (auditChannel?.isSendable()) {
+              await auditChannel.send({
+                content: `Message from ${actualMention(
+                  member.user,
+                )} at <t:${Math.round(
+                  message.createdTimestamp / 1000,
+                )}> contained a Discord invite!
+Invites: \`${matches.join(", ")}\``,
+                allowedMentions: { users: [] },
+              });
+            }
+          } catch (error) {
+            console.error(
+              "Failed to delete message with Discord invite:",
+              error,
+            );
+          }
+        }
+      },
+      async messageUpdate(_, oldMessage, message) {
+        if (message.author.bot || !message.inGuild()) return;
+        const member = await getMember(message);
+        if (!member) return; // || isSpecialUser(member) ignore for now
+        if (await isAllowedToSendDiscordInvites(member)) return;
+
+        // Check if message contains any Discord invite
+        const matches = invitePatterns
+          .map((pattern) => message.content.match(pattern))
+          .filter((match) => match != null && match.length > 0)
+          .map((match) => match![0])
+          .filter(
+            (match) =>
+              !whitelistDomains.some((domain) => match.includes(domain)),
+          );
+
+        const hasInvite = matches.length > 0;
+        console.log(hasInvite, matches);
+
+        if (hasInvite) {
+          try {
+            await message.delete();
+
+            const warningMessage = await message.channel.send({
+              content: `${actualMention(member)}, only Users with Tier 2 or over are allowed to send Discord invites.\nPlease remove the invite before sending it again.\nThank you!`,
+            });
+
+            setTimeout(() => {
+              warningMessage.delete().catch(() => {});
+            }, 10000);
+
+            const auditChannel = await message.guild.channels.fetch(
+              config.channels.auditLog,
+            );
+
+            if (auditChannel?.isSendable()) {
+              await auditChannel.send({
+                content: `Message from ${actualMention(
+                  member.user,
+                )} at <t:${Math.round(
+                  message.createdTimestamp / 1000,
+                )}> contained a Discord Invite\n(It was edited to contain a Discord invite)!
+Invites: \`${matches.join(", ")}\``,
+                allowedMentions: { users: [] },
+              });
+            }
+          } catch (error) {
+            console.error(
+              "Failed to delete message with Discord invite:",
+              error,
+            );
+          }
+        }
+      },
+    },
+  ],
+};
