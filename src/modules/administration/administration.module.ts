ba9f9ba709e27e50a788189ef321b077f2c5a095
Pdzly <rooki@rooki.xyz>
2025-08-25 19:52:32 +0200

    Move `ban` and `unban` commands from `AdministrationModule` to `ModerationModule`

--- src/modules/administration/administration.module.ts
+++
@@ -1,8 +0,0 @@
-import Module from "../module.js";
-import { BanCommand } from "./ban.command.js";
-import { UnbanCommand } from "./unban.command.js";
-
-export const AdministrationModule: Module = {
-  name: "administration",
-  commands: [BanCommand, UnbanCommand],
-};
