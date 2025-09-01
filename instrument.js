2c846fa20635bb73e0e1623301b2742ba71e8979
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-09-01 23:01:03 +0100

    run treefmt

--- instrument.js
+++ instrument.js
@@ -3,16 +3,16 @@ import * as Sentry from "@sentry/bun";
 console.log("Starting Sentry Profiling Integration");
 // Ensure to call this before importing any other modules!
 Sentry.init({
-  dsn: process.env.DDB_SENTRY_DSN,
-  sendDefaultPii: true,
-  release: process.env.npm_package_version ?? process.env.VERSION ?? "unknown",
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 0.2,
-  integrations: [
-    Sentry.extraErrorDataIntegration(),
-    Sentry.onUncaughtExceptionIntegration(),
-    Sentry.onUnhandledRejectionIntegration(),
-    Sentry.fsIntegration(),
-    Sentry.postgresIntegration(),
-  ],
+	dsn: process.env.DDB_SENTRY_DSN,
+	sendDefaultPii: true,
+	release: process.env.npm_package_version ?? process.env.VERSION ?? "unknown",
+	tracesSampleRate: 1.0,
+	profilesSampleRate: 0.2,
+	integrations: [
+		Sentry.extraErrorDataIntegration(),
+		Sentry.onUncaughtExceptionIntegration(),
+		Sentry.onUnhandledRejectionIntegration(),
+		Sentry.fsIntegration(),
+		Sentry.postgresIntegration(),
+	],
 });
