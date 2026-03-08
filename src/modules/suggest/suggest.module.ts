8ed948f4e698327b4574c86945881bd03550c739
Pdzly <34943569+Pdzly@users.noreply.github.com>
2026-03-08 20:31:23 +0100

    Add suggestion vote ban reconciliation command

--- src/modules/suggest/suggest.module.ts
+++ src/modules/suggest/suggest.module.ts
@@ -2,10 +2,15 @@ import type Module from "../module.js";
 import { ManageSuggestionCommand } from "./manageSuggestion.command.js";
 import { SuggestCommand } from "./suggest.command.js";
 import { SuggestionButtonListener } from "./suggest.listener.js";
+import { SyncSuggestionVotesCommand } from "./syncSuggestionVotes.command.js";
 
 export const SuggestModule: Module = {
 	name: "suggest",
-	commands: [SuggestCommand, ManageSuggestionCommand],
+	commands: [
+		SuggestCommand,
+		ManageSuggestionCommand,
+		SyncSuggestionVotesCommand,
+	],
 	listeners: [SuggestionButtonListener],
 };
 
